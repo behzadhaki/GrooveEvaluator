1179566d7d7d84247e41db1ce8d69eb9b6af2970
Teresa Pelinski <teresapelinski@gmail.com>
2021-06-07 14:21:18 +0200

    fixed paths

--- test/evaluator_test.py
+++ test/evaluator_test.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.insert(1, "../../")
-sys.path.insert(1, "../")
+sys.path.insert(1, "../../hvo_sequence")
+sys.path.insert(1, "../hvo_sequence")
 
 from hvo_sequence.drum_mappings import ROLAND_REDUCED_MAPPING
 import wandb
@@ -10,10 +10,10 @@ from GrooveEvaluator.evaluator import Evaluator
 
 from bokeh.io import output_file, show, save
 
-wandb_run = wandb.init(project="GMD Analysis3", entity="behzadhaki")
+wandb_run = wandb.init(project="GMD Analysis3")
 
-pickle_source_path = "../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.3/" \
-                     "Processed_On_04_06_2021_at_17_38_hrs"
+pickle_source_path = "../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.3" \
+                     "/Processed_On_04_06_2021_at_17_38_hrs"
 
 # Create Subset Filters (styles both in train and test)
 styles = ["hiphop", "funk", "reggae", "soul", "latin", "jazz", "pop", "afrobeat", "highlife", "punk", "rock"]
@@ -63,7 +63,8 @@ rhythmic_distances = train_set_evaluator.get_rhythmic_distances()
 wandb.log(rhythmic_distances, commit=False)
 
 # Get Heatmaps/Features per gt or predicted
-heatmaps_global_features = train_set_evaluator.get_wandb_logging_media()
+heatmaps_global_features = train_set_evaluator.get_wandb_logging_media(sf_paths=[
+    "../../hvo_sequence/hvo_sequence/soundfonts/Standard_Drum_Kit.sf2"])
 if len(heatmaps_global_features.keys()) > 0:
     wandb.log(heatmaps_global_features, commit=False)
 
@@ -75,7 +76,8 @@ train_set_evaluator.dump(path="misc/evaluator_run_{}_Epoch_{}.Eval".format(wandb
 
 # Pass to model
 # predicted_hvos_array = model.predict(gt_hvos_array)
-gt_log_dict, predicted_log_dict = train_set_evaluator.get_logging_dict()
+gt_log_dict, predicted_log_dict = train_set_evaluator.get_logging_dict(sf_paths=[
+    "../../hvo_sequence/hvo_sequence/soundfonts/Standard_Drum_Kit.sf2"])
 gt_log_dict.keys()
 train_set_evaluator.add_predictions(gt_hvos_array)
 
