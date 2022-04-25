6fa06a60b64e1712b461e9de2a16d61976c81097
Teresa Pelinski <teresapelinski@gmail.com>
2022-04-25 12:44:16 +0200

    removed __all__

--- __init__.py
+++ __init__.py
@@ -1,4 +1,1 @@
 from .GrooveEvaluator import Evaluator, HVOSeq_SubSet_Evaluator, separate_figues_by_tabs, get_stats_from_evaluator
-
-__all__ = [Evaluator, HVOSeq_SubSet_Evaluator,
-           separate_figues_by_tabs, get_stats_from_evaluator]
